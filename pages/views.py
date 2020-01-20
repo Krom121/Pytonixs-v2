43c84a2bbfd69946bd8427f893021648b4b900c9
stephen <stephenmacgregor9@gmail.com>
2020-01-20 09:55:10 +0000

    pages-app/views/blog-urls

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.core.mail import send_mail
+from django.views.generic import FormView ,ListView, TemplateView
+from .models import NewClient
+from .forms import ContactForm
+from blog.models import Post
 
-# Create your views here.
+
+class HomeView(FormView,TemplateView):
+    form_class = ContactForm
+    template_name = 'welcome/index.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        form.save()
+        return super(HomeView, self).form_valid(form)
+        
+class AboutView(TemplateView):
+    template_name = 'about_us/about.html'
+
+
+class ServiceView(TemplateView):
+    template_name = 'what_we_do/service.html'
+
+
+class ProjectView(TemplateView):
+    template_name = 'completed_projects/projects.html'
+
+
+class ContactView(FormView):
+    form_class = ContactForm
+    template_name = 'form/contact.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        form.save()
+        return super(ContactView, self).form_valid(form)
--- pytonixs/urls.py
+++ pytonixs/urls.py
@@ -2,9 +2,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from pages.views import AboutView, HomeView, ProjectView, ServiceView, ContactView
+from blog.views import PostListView, PostDetailView
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('batmans-admin/', admin.site.urls),
+    ### PAGES APP URLS ###
+    path('', HomeView.as_view(), name='home'),
+    path('about_us/', AboutView.as_view(), name='about'),
+    path('what_we_do/', ServiceView.as_view(), name='service'),
+    path('completed_projects/', ProjectView.as_view(), name='project'),
+    path('contact_us/', ContactView.as_view(), name='contact'),
+    #### BLOG URLS ###
+    path('blog', PostListView.as_view(), name='list'),
+    path('post/<slug>/<pk>/', PostDetailView.as_view(), name='post-detail'),
     ## OTHER BASE URLS FOR DEPENDENCIES #####
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('api-auth/', include('rest_framework.urls')),
